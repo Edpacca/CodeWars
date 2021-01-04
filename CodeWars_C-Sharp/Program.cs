6cc935f63c615a3a9d997946df7031f52607f5c1
Edpacca <eddie@pace.me.uk>
2021-01-04 11:28:06 +0000

    main program commit

---
+++ CodeWars_C-Sharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CodeWars
+{
+    public static partial class Kata
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
