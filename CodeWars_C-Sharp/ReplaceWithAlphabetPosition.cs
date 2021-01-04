7b79a3d50f1344d50aad91779b8c9cafca8afac9
Edpacca <eddie@pace.me.uk>
2021-01-04 11:30:58 +0000

    6 kyu kata solutions

---
+++ CodeWars_C-Sharp/ReplaceWithAlphabetPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+namespace CodeWars
+{
+    class ReplaceWithAlphabetPosition
+    {
+        public static string AlphabetPosition(string text)
+        {
+            var positions = new StringBuilder();
+
+            foreach (var letter in text.ToLower())
+            {
+                if ((int)letter >= 97 && (int)letter <= 122)
+                    positions
+                        .Append((int)letter - 96)
+                        .Append(" ");
+            }
+
+            return positions.ToString();
+        }
+    }
+}
---
+++ CodeWars_C-Sharp/TribonacciSequence.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+// Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+// Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array(except in C return NULL) and be ready for anything else which is not clearly specified ;)
+
+namespace CodeWars
+{
+    class TribonacciSequence
+    {
+        public static double[] Tribonacci(double[] signature, int n)
+        {
+            var tribonaccified = new double[n];
+
+            if (n == 0)
+                return tribonaccified;
+
+            else if (n < 3)
+            {
+                for (int i = 0; i < n; i++)
+                    tribonaccified[i] = signature[i];
+
+                return tribonaccified;
+            }
+
+            for (int i = 0; i < 3; i++)
+                tribonaccified[i] = signature[i];
+
+            for (int i = 3; i < n; i++)
+                tribonaccified[i] = tribonaccified.ToList().Skip(i - 3).Take(3).Sum();
+
+            return tribonaccified;
+        }
+    }
+}
