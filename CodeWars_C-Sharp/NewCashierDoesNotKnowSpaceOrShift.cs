b7ab05c341648bce595cd166eec9521e7198f347
Edpacca <eddie@pace.me.uk>
2021-01-05 16:03:51 +0000

    6 kyu kata

---
+++ CodeWars_C-Sharp/NewCashierDoesNotKnowSpaceOrShift.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWars
+{
+    class NewCashierDoesNotKnowSpaceOrShift
+    {
+        static List<string> orders = new List<string>
+        {
+            "Burger",
+            "Fries",
+            "Chicken",
+            "Pizza",
+            "Sandwich",
+            "Onionrings",
+            "Milkshake",
+            "Coke"
+        };
+
+        public static string GetOrder(string input)
+        {
+            StringBuilder formattedOrder = new StringBuilder();
+
+            string buffer = input;
+
+            foreach (var item in orders)
+            {
+                string itemLowerCase = item.ToLower();
+
+                while (buffer.Contains(itemLowerCase))
+                {
+                    formattedOrder.Append(item + " ");
+                    buffer = buffer.Remove(input.IndexOf(itemLowerCase), itemLowerCase.Length);
+                }
+            }
+
+            return formattedOrder.ToString().Trim();
+        }
+    }
+}
