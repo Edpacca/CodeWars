f4694ea28f581188eb62770c5d2a8a96ee3fbac9
Edpacca <eddie@pace.me.uk>
2021-01-05 15:33:28 +0000

    7th kyu kata

---
+++ CodeWars_C-Sharp/SumLowestPositiveIntegers.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace CodeWars
+{
+    class SumLowestPositiveIntegers
+    {
+        public static int SumTwoSmallestNumbers(int[] numbers)
+        {
+            return numbers.OrderBy(x => x).Take(2).Sum();
+        }
+
+    }
+}
